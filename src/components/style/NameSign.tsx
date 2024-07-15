50443328ed1771f87c83b0702c9a37c32e42b030
Owen Wexler <owenbwexler@gmail.com>
2024-07-15 01:06:40 -0400

    Convert logo to Astro Image and add loading spinner to name card

--- src/components/style/NameSign.tsx
+++ src/components/style/NameSign.tsx
@@ -1,4 +1,5 @@
 import type { FunctionComponent } from "preact";
+import Spinner from "./Spinner";
 
 interface NameSignProps {
   name: string;
@@ -7,7 +8,13 @@ interface NameSignProps {
 const NameSign: FunctionComponent<NameSignProps> = ({ name }) => {
   return (
     <div className="flex flex-col items-center justify-center text-center max-w-full py-6 max-sm:py-4 px-16 rounded-lg bg-yellow-600 border border-blue-600">
-      <p className="text-3xl text-white font-bold">{name}</p>
+      {
+        name === ''
+        ?
+          <Spinner />
+        :
+          <p className="text-3xl text-white font-bold">{name}</p>
+      }
     </div>
   )
 }
