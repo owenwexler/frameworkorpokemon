8fa738425991dfa427f5d2d2cfcf8d84c1e90f6f
Owen Wexler <owenbwexler@gmail.com>
2024-07-26 02:42:37 -0400

    Install ioredis and add redis cache in front of database

--- db/models.ts
+++ db/models.ts
@@ -2,10 +2,21 @@ import { db } from "./db";
 import type { Item } from "../typedefs/Item";
 import { items } from "./schema";
 
+import redis from './redis/redis';
+
+const CACHE_KEY = 'fop::data';
+
 const getAllItems = async (): Promise<Item[]> => {
   try {
-    const result = await db.select().from(items);
-    return result as Item[];
+    const cacheResponse = await redis.get(CACHE_KEY);
+
+    if (cacheResponse && !cacheResponse.hasOwnProperty('error')) {
+      return JSON.parse(cacheResponse) as Item[];
+    } else {
+      const result = await db.select().from(items);
+      redis.set(CACHE_KEY, JSON.stringify(result), 'EX', 60 * 60 * 60);
+      return result as Item[];
+    }
   } catch(err) {
     console.error(err);
     throw err;
