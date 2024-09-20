bd206f602ae6103effd101a5ea4f4526b8ac8f43
Owen Wexler <owenbwexler@gmail.com>
2024-09-20 18:51:15 -0400

    Add Node integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  server: { port: 3000, host: true }
+  server: {
+    port: 3000,
+    host: true
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
